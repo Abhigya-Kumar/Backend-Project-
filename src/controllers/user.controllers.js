1efd25c2962d5e84aa9a55d6d438aca4f60db455
Abhigya Kumar <abhigya878@gmail.com>
2026-02-10 18:03:10 +0530

    completed user model with testing

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,6 +2,7 @@ import { User } from "../models/user.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import {asyncHandler} from "../utils/asyncHandler.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler( async (req,res) => {
     // get details from the user
@@ -15,8 +16,10 @@ const registerUser = asyncHandler( async (req,res) => {
     // return res
 
 
+
+
     const {fullName, email, userName, password} = req.body
-    console.log("email :", email);
+
 
     if (
         [fullName,email,userName,password]
@@ -25,7 +28,7 @@ const registerUser = asyncHandler( async (req,res) => {
      throw new ApiError(400, "All feilds are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{email}, {userName}]
     })
 
@@ -34,7 +37,11 @@ const registerUser = asyncHandler( async (req,res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
@@ -53,7 +60,7 @@ const registerUser = asyncHandler( async (req,res) => {
         coverImage: coverImage?.url || "",
         email,
         password,
-        username: username.toLowerCase()
+        username: userName.toLowerCase()
     })
 
     const createdUser = await User.findById(user._id)
@@ -62,6 +69,10 @@ const registerUser = asyncHandler( async (req,res) => {
     if (!createdUser) {
         throw new ApiError(500, "Something went wrong while registering the user")
     }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "user registered succesfully")
+    )
 
 })
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -3,9 +3,9 @@ import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
 
 cloudinary.config({ 
-    cloud_name: 'process.env.CLOUDINARY_CLOUD_NAME', 
-    api_key: 'process.env.CLOUDINARY_API_KEY', 
-    api_secret: 'process.env.CLOUDINARY_API_SECRET' 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
 const uploadOnCloudinary = async (localfilepath) => {
@@ -16,9 +16,10 @@ const uploadOnCloudinary = async (localfilepath) => {
         const response = await cloudinary.uploader.upload(localfilepath,{
             resource_type: "auto",
         })
-        console.log("file uploaded sucessfully on cloudinary", 
-            response.url
-        );
+        // console.log("file uploaded sucessfully on cloudinary", 
+        //     response.url
+        // );
+        fs.unlinkSync(localfilepath)
         return response;
     } catch (error) {
         fs.unlinkSync(localfilepath) // remove local file temp stored
